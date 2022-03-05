981dc5d365e985b30c6342c123caee89bbc8b888
fredrik Andersson <fandersson1121@gmail.com>
2022-03-05 18:33:42 +0100

    add category matches for Help Screen

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,10 @@ const request = require("sync-request");
 const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
 const { json } = require("express");
+const { populate } = require("../models/users");
 
 //!  INTERACTION SCREEN - en GET
 router.get("/get-matches/:token", async (req, res) => {
-  console.log("got fetch");
   const { token } = req.params;
   //get current User
   let currentUser = await UserModel.findOne({ token: token });
@@ -54,7 +54,6 @@ router.get("/get-matches/:token", async (req, res) => {
 
 //!  ASK SCREEN - en GET
 router.get("/get-willing-users/:token", async (req, res) => {
-  console.log("got fetch");
   const { token } = req.params;
   //get current User
   let currentUser = await UserModel.findOne({ token: token });
@@ -120,7 +119,6 @@ router.put("/accept-helper/:reqId/:token", async (req, res) => {
     );
     foundRequest.willing_users = willing_users;
     let userToAdd = await UserModel.findOne({ token: token });
-    console.log(userToAdd);
     foundRequest.accepted_users.push(userToAdd._id);
     let savedRequest = await foundRequest.save();
     res.json({ status: true, request: savedRequest });
@@ -129,16 +127,39 @@ router.put("/accept-helper/:reqId/:token", async (req, res) => {
   }
 });
 
+//! MATCH CATEGORIES
+router.get("/match-categories/:token", async (req, res) => {
+  const { token } = req.params;
+
+  let foundUser = await UserModel.findOne({ token: token }).populate(
+    "categories"
+  );
+
+  if (foundUser.categories != 0) {
+    let allRequests = [];
+    let requests = await RequestModel.find({
+      $and: [{ category: foundUser.categories }, { helper: null }],
+    })
+      .populate("asker")
+      .populate("category")
+      .populate({
+        path: "asker",
+        populate: {
+          path: "categories",
+          model: "categories",
+        },
+      });
+
+    res.json({ status: true, matchingRequests: requests });
+  } else {
+    res.json({ status: false, message: "Vous n'avez pas encore de categorie" });
+  }
+});
+
 //! SUGGESTIONS EN GET
 //
 // ─── /suggestions EN GET ─────────────────────────────────────────────────────────
 // HOME SCREEN : carroussel du Dashbord. (find sur toutes les categories s=dont les suggestions sont à "true")
-
-//! MATCH CATEGORIES
-//
-// ─── /match-categories ─────────────────────────────────────────────────────────
-// HELPS CREEN : route en GET sur catégories, avec fetch en get avec url de la route matchcatgories côté front
-// + pastille helper selected ou non [selected_users] du requestSchema
 
 //! addwilling_user
 //
